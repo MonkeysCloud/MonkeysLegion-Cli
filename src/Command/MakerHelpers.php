3a3c0a2c841b362b76fca0973bf80a5d536b60a8
Amanar Marouane <marouane.amanar07@gmail.com>
2025-08-03 16:59:43 +0100

    fix: remove not used confirmable trait along with fixes on MakeEntityCommand

--- src/Command/MakerHelpers.php
+++ src/Command/MakerHelpers.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 namespace MonkeysLegion\Cli\Command;
@@ -6,14 +7,14 @@ namespace MonkeysLegion\Cli\Command;
 // Helper trait for common ask/fail methods
 trait MakerHelpers
 {
-    private function ask(string $prompt): string
+    protected function ask(string $prompt): string
     {
         return function_exists('readline')
             ? trim(readline("$prompt ") ?: '')
             : trim(fgets(STDIN) ?: '');
     }
 
-    private function fail(string $msg): int
+    protected function fail(string $msg): int
     {
         $this->error($msg);
         return self::FAILURE;
